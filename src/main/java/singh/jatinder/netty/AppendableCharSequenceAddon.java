5c6e291fc2ad24ffebde7b7e0c9477b64a475fd6
Jatinder <jatinder@mediaiqdigital.com>
2015-03-31 17:03:38 +0530

    Initialization fixes

--- src/main/java/singh/jatinder/netty/AppendableCharSequenceAddon.java
+++ src/main/java/singh/jatinder/netty/AppendableCharSequenceAddon.java
@@ -3,13 +3,13 @@
  */
 package singh.jatinder.netty;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.CtNewMethod;
+import javassist.bytecode.DuplicateMemberException;
 
 /**
  * @author Jatinder
@@ -19,21 +19,23 @@ public final class AppendableCharSequenceAddon {
     private static Method subsequence;
     static {
         try {
+            Class clazz = null;
             CtClass ctclass = ClassPool.getDefault().get("io.netty.util.internal.AppendableCharSequence");
-            CtMethod newmethod = CtNewMethod.make("public void subSequence(int start, int end, io.netty.util.internal.AppendableCharSequence seq) { if(seq.chars.length<end-start) { seq.chars = seq.expand(seq.chars, end-start, seq.chars.length); } seq.pos = end-start; System.arraycopy(chars, start, seq.chars, 0, seq.pos);}",ctclass);
-            ctclass.addMethod(newmethod);
-            ctclass.writeFile();
-
-            /*for(Method me: ctclass.toClass().getDeclaredMethods()){ //test print, ok
-                System.out.print(me.getName() + "\t");
-                for (Class clazz :me.getParameterTypes()) {
-                    System.out.print(clazz.getCanonicalName()+ "\t");
-                }
-                System.out.println();
-            }*/
-            subsequence = ctclass.toClass().getDeclaredMethod("subSequence", int.class, int.class, io.netty.util.internal.AppendableCharSequence.class);
+            try {
+                CtMethod newmethod = CtNewMethod.make("public void subSequence(int start, int end, io.netty.util.internal.AppendableCharSequence seq) { if(seq.chars.length<end-start) { seq.chars = seq.expand(seq.chars, end-start, seq.chars.length); } seq.pos = end-start; System.arraycopy(chars, start, seq.chars, 0, seq.pos);}",ctclass);
+                ctclass.addMethod(newmethod);
+                ctclass.writeFile();
+                clazz = ctclass.toClass();
+            } catch (DuplicateMemberException dme) {
+                dme.printStackTrace();
+            }
+            if (null==clazz) {
+                clazz = io.netty.util.internal.AppendableCharSequence.class;
+            }
+            subsequence = clazz.getDeclaredMethod("subSequence", int.class, int.class, io.netty.util.internal.AppendableCharSequence.class);
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("AppendableCharSequence Injection/Initialization failed");
         }
     }
     
